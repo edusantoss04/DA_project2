5f81ae397590ca6a801c3a1ccf7dff062d9c1d5b
PP <pedrosspedro@sapo.pt>
2024-05-08 12:33:40 +0100

    DataManip

---
+++ src/DataManip.cpp
@@ -0,0 +1,3 @@
+#include "DataManip.h"
+
+DataManip::DataManip() {}
---
+++ src/DataManip.h
@@ -0,0 +1,14 @@
+#ifndef DA_PROJECT2_DATAMANIP_H
+#define DA_PROJECT2_DATAMANIP_H
+
+
+class DataManip {
+private:
+    Graph graph{};
+
+public:
+    DataManip();
+};
+
+
+#endif //DA_PROJECT2_DATAMANIP_H
